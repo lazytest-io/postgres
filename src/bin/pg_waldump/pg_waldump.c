5c8fa932a210130f98c1a539aeab7094eb1c8a35
Paul Johnson <paul@johnson.kiwi.nz>
2021-05-26 10:26:50 -0700

    Formatting fixes

--- src/bin/pg_waldump/pg_waldump.c
+++ src/bin/pg_waldump/pg_waldump.c
@@ -558,19 +558,16 @@ XLogDumpDisplayRecord(XLogDumpConfig *config, XLogReaderState *record)
 	// 	}
 	// }
 
-if (strcmp(desc->rm_name, "LogicalMessage") == 0) {
-  rec_data = XLogRecGetData(record);
-  xlrec = (xl_logical_message *) rec_data;
-  message = xlrec->message + xlrec->prefix_size;
-  printf("{\"transaction_id\": %10u,", XLogRecGetXid(record));
-  printf("\"transactional\": %s", xlrec->transactional ? "true" : "false");
-  printf("\"lsn\": %X/%08X,", (uint32) (record->ReadRecPtr >> 32), (uint32) record->ReadRecPtr);
-  printf("\"message\": %.*s}", xlrec->message_size, message);
-  putchar('\n');
-}
-}
-
-/*
+  if (strcmp(desc->rm_name, "LogicalMessage") == 0) {
+    rec_data = XLogRecGetData(record);
+    xlrec = (xl_logical_message *) rec_data;
+    message = xlrec->message + xlrec->prefix_size;
+    printf("{\"transactionId\": %10u,", XLogRecGetXid(record));
+    printf("\"transactional\": %s, ", xlrec->transactional ? "true" : "false");
+    printf("\"lsn\": \"%X/%08X\", ", (uint32) (record->ReadRecPtr >> 32), (uint32) record->ReadRecPtr);
+    printf("\"message\": %.*s}", xlrec->message_size, message);
+    putchar('\n');
+  }
 }
 
 /*
